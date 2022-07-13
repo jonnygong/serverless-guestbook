3a4ce7f0aa1e73a6ada960c47ec35b9abe1b1c5e
JonnyGong <850954932@qq.com>
2022-07-13 17:21:07 +0800

    Update api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7058544e.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
